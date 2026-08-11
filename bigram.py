85993af67f73ba6f7c276a016700c4169c14a468
tangdarren <tang.darren@gmail.com>
2026-08-11 14:52:01 -0700

    feat: tokenize corpus and create data splits

--- bigram.py
+++ bigram.py
@@ -1,3 +1,5 @@
+import torch
+
 # read the tiny shakespeare dataset
 with open('input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
@@ -12,3 +14,23 @@ chars = sorted(list(set(text)))
 vocab_size = len(chars)
 print(''.join(chars))
 print(vocab_size)
+
+# create a mapping from characters to integers
+stoi = { ch:i for i,ch in enumerate(chars) }
+itos = { i:ch for i,ch in enumerate(chars) }
+encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
+decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
+
+print(encode("hii there"))
+print(decode(encode("hii there")))
+
+# let's now encode the entire text dataset and store it into a torch.Tensor
+data = torch.tensor(encode(text), dtype=torch.long)
+print(data.shape, data.dtype)
+print(data[:1000]) # the 1000 characters we looked at earlier will look like this to the GPT
+
+# Let's now split up the data into train and validation sets
+n = int(0.9*len(data)) # first 90% will be train, rest val
+train_data = data[:n]
+val_data = data[n:]
+print(len(train_data), len(val_data))
