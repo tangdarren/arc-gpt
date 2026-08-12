3f45e5dc82f9ee050d7451c271511e5c8d994424
tangdarren <tang.darren@gmail.com>
2026-08-12 15:42:20 -0700

    feat: add feed-forward transformer layers

--- gpt.py
+++ gpt.py
@@ -119,3 +119,20 @@ out = mha(x)
 print(num_heads)
 print(head_size)
 print(out.shape)
+
+class FeedForward(nn.Module):
+
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, n_embd),
+            nn.ReLU(),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+ffwd = FeedForward(32)
+print(out.shape)
+out = ffwd(out)
+print(out.shape)
